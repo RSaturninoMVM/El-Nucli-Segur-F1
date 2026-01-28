cfb4b296eda5a024db5ff944ecc99c43dc37cbf8
RSaturninoMVM <rsaturnino@institutmvm.cat>
2026-01-28 10:16:43 +0100

    Add files via upload

---
+++ future_motors.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def __init__(self, matricula, model, kms_inicials):
+        self.matricula = matricula
+        self._model = model       # Protegit (un guió)
+        self.__kms = kms_inicials # Privat (dos guions)
+
+    # TASCA: Fes el Getter per llegir els kms (ja que és privat)
+    def llegir_kms(self):
+        print("Calculando los kilometros")
+        return self.__kms
+        pass
+
+    # TASCA: Fes el Setter amb seguretat anti-frau
+    def actualitzar_kms(self, nous_kms):
+        # Si nous_kms és més petit que self.__kms, retorna False.
+        if nous_kms < self.__kms:
+            return False
+        # Si és correcte, actualitza i retorna True.
+        else:
+            return True
+        pass
